afe5bf53d011a72f5c542e8ef0510dbcc007d356
ayet <ayetayadi01@gmail.com>
2024-04-27 12:01:45 +0100

    Modified

--- app.js
+++ app.js
@@ -1,10 +1,93 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var contacts = localStorage.getItem('contacts');
     var contactsList = document.querySelector('.list-contact');
     var noContactDiv = document.querySelector('.no-contact');
+    var ajouterContactBtn = document.getElementById('ajouter-contact-btn');
+    var formulaireContact = document.getElementById('formulaire-contact');
+    var enregistrerBtn = document.getElementById('enregistrer-btn');
+    var effacerBtn = document.getElementById('effacer-btn');
+    var supprimerContactsBtn = document.getElementById('supprimer-contacts-btn');
 
-    
-    //Vérifier s'il y'a des contacts 
+    // Ajouter un contact
+    ajouterContactBtn.addEventListener('click', function () {
+        formulaireContact.style.display = 'block';
+    });
+
+    // Ajouter le nouveau contact avec le bouton enregistrer
+    enregistrerBtn.addEventListener('click', function () {
+        var civilite = document.getElementById('civilite').value;
+        var prenom = document.getElementById('prenom').value;
+        var nom = document.getElementById('nom').value;
+        var telephone = document.getElementById('telephone').value;
+
+        if (civilite === '' || prenom === '' || nom === '' || telephone === '') {
+            alert('Veuillez remplir tous les champs.');
+            return;
+        }
+
+        var contactId = formulaireContact.getAttribute('data-contact-id');
+
+        var contacts = localStorage.getItem('contacts') ? JSON.parse(localStorage.getItem('contacts')) : [];
+
+        var contactExists = contacts.some(function (contact) {
+            return contact.prenom.toUpperCase() === prenom.toUpperCase() && contact.nom.toUpperCase() === nom.toUpperCase();
+        });
+
+        if (contactExists) {
+            alert("Ce contact existe déjà.");
+            return;
+        }
+
+        // Si un id de contact existe, mise à jour des données du contact existant
+        if (contactId) {
+            contacts.forEach(function (contact) {
+                if (contact.id === parseInt(contactId)) {
+                    contact.civilite = civilite;
+                    contact.prenom = prenom;
+                    contact.nom = nom;
+                    contact.telephone = telephone;
+                }
+            });
+        } else {
+            // Sinon, ajout d'un nouveau contact avec un nouvel identifiant généré
+            var maxId = contacts.reduce((max, contact) => Math.max(max, contact.id), 0);
+            var id = maxId + 1;
+
+            var nouveauContact = {
+                id: id,
+                civilite: civilite,
+                prenom: prenom,
+                nom: nom,
+                telephone: telephone
+            };
+
+            contacts.push(nouveauContact);
+        }
+
+        localStorage.setItem('contacts', JSON.stringify(contacts));
+
+        displayContacts(contacts);
+
+        formulaireContact.style.display = 'none';
+    });
+
+    // Effacer un contact
+    effacerBtn.addEventListener('click', function () {
+        // Reinitialiser des champs du formulaire
+        document.getElementById('civilite').value = 'Madame';
+        document.getElementById('prenom').value = '';
+        document.getElementById('nom').value = '';
+        document.getElementById('telephone').value = '';
+
+        // Affichage des contacts existants
+        var storedContacts = localStorage.getItem('contacts');
+        if (storedContacts) {
+            var contacts = JSON.parse(storedContacts);
+            displayContacts(contacts);
+        }
+    });
+
+    // Vérification de l'existence de contacts et affichage de la liste des contacts en conséquence
     if (contacts) {
         contacts = JSON.parse(contacts);
         if (contacts.length > 0) {
@@ -19,40 +102,109 @@ document.addEventListener('DOMContentLoaded', function() {
         contactsList.style.display = 'none';
         noContactDiv.style.display = 'block';
     }
-});
 
-function displayContacts(contacts) {
-    var contactsList = document.querySelector('.list-contact');
-    contactsList.innerHTML = ''; 
+    // Suppression des contacts
+    supprimerContactsBtn.addEventListener('click', function () {
+        localStorage.removeItem('contacts');
 
-    contacts.forEach(function(contact) {
-        var contactItem = document.createElement('div');
-        contactItem.classList.add('contact-item');
-
-        // Création de l'élément d'icône et ajout de classe et de contenu
-        var icon = document.createElement('img');
-        icon.classList.add('contact-icon');
-        icon.src = 'C:/Users/ayeta/Downloads/icons8-utilisateur-24 (1).png';
-        icon.alt = 'Icon';
-        contactItem.appendChild(icon);
-
-        // Ajout du nom et du prénom
-        var nameElement = document.createElement('p');
-        nameElement.textContent = contact.name + ' ' + contact.surname;
-        contactItem.appendChild(nameElement);
-
-        contactsList.appendChild(contactItem);
+        contactsList.style.display = 'none';
+        noContactDiv.style.display = 'block';
     });
-}
 
+    // Fonction pour afficher les contacts
+    function displayContacts(contacts) {
+        // Tri des contacts par ordre alphabétique du nom, puis du prénom
+        contacts.sort(function (a, b) {
 
-// Créez un tableau de contacts factices
-var contacts = [
-    { name: 'John', surname: 'Doe', phone: '123456789' },
-    { name: 'Jane', surname: 'Smith', phone: '987654321' }
-];
+            var nom1 = a.nom.toUpperCase();
+            var nom2 = b.nom.toUpperCase();
+            if (nom1 < nom2) {
+                return -1;
+            }
+            if (nom1 > nom2) {
+                return 1;
+            }
+            // Si les noms de famille sont égaux, comparaison par prénom
+            var prenom1 = a.prenom.toUpperCase();
+            var prenom2 = b.prenom.toUpperCase();
+            if (prenom1 < prenom2) {
+                return -1;
+            }
+            if (prenom1 > prenom2) {
+                return 1;
+            }
+            return 0;
+        });
 
-// Convertissez le tableau en chaîne JSON et enregistrez-le dans le localStorage sous la clé 'contacts'
-localStorage.setItem('contacts', JSON.stringify(contacts));
+        contactsList.innerHTML = '';
 
+        contacts.forEach(function (contact) {
+            var contactItem = document.createElement('div');
+            contactItem.classList.add('contact-item');
 
+            var icon = document.createElement('img');
+            icon.classList.add('contact-icon');
+            icon.src = 'C:/Users/ayeta/Downloads/icons8-utilisateur-24 (1).png';
+            icon.alt = 'Icon';
+            contactItem.appendChild(icon);
+
+            var nameElement = document.createElement('p');
+            nameElement.textContent = contact.prenom + ' ' + contact.nom;
+            contactItem.appendChild(nameElement);
+
+            // Affichage les détails du contact
+            contactItem.addEventListener('click', function () {
+                var allContactItems = document.querySelectorAll('.contact-item');
+                allContactItems.forEach(function (item) {
+                    item.classList.remove('selected');
+                });
+
+                formulaireContact.style.display = 'none';
+
+                contactItem.classList.add('selected');
+
+                // Affichage de details du contact
+                var detailsBox = document.querySelector('.contact-details');
+                detailsBox.innerHTML = `
+                    <h2>Détail du Contact</h2>
+                    <p>${contact.civilite} ${contact.prenom} ${contact.nom}</p>
+                    <p>Tel: ${contact.telephone}</p>
+                    <button id="edit-contact-btn" data-contact-id="${contact.id}"><span>Editer le contact</span></button>
+                `;
+
+                // Affichage de la boîte des détails du contact
+                detailsBox.style.display = 'block';
+                
+            });
+
+            // Ajout de l'élément de contact à la liste des contacts
+            contactsList.appendChild(contactItem);
+        });
+
+        //  Si on clique dehors
+        document.addEventListener('click', function (event) {
+            var detailsBox = document.querySelector('.contact-details');
+            if (!detailsBox.contains(event.target) && !contactsList.contains(event.target)) {
+                detailsBox.style.display = 'none';
+                var allContactItems = document.querySelectorAll('.contact-item');
+                allContactItems.forEach(function (item) {
+                    item.classList.remove('selected');
+                });
+            }
+        });
+    }
+
+    // Fonction pour ajuster la taille de l'élément .contacts
+    function tailleContacts() {
+        var contactsElement = document.querySelector('.contacts');
+        var formulaireContact = document.getElementById('formulaire-contact');
+        var initialContactsHeight = contactsElement.offsetHeight;
+        var contactDetailsElement = document.querySelector('.contact-details');
+        if (formulaireContact.style.display === 'block' || contactDetailsElement.style.display === 'block') {
+            contactsElement.style.height = initialContactsHeight + 'px';
+        } else {
+            contactsElement.style.height = initialContactsHeight + 'px';
+        }
+    }
+    tailleContacts();
+});
